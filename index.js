ba4119525cf6c84c721a7201eb779e8eaf1a65fe
Valerio <viale.valerio@gmail.com>
2023-02-14 13:03:44 +0100

    deleted body parser, added comments to index.js

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-//declaring const express, path, bodyParser, app
+//This is a Node.js file that creates an API server using the Express framework. 
+//Here is a brief overview of what the code does:
+//Imports the required dependencies: dotenv for loading environment variables, 
 require('dotenv').config();
+//Express for creating the server, path for handling file paths, body-parser for parsing request bodies, 
+//(declaring const express, path)
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const app = express();
 
-
-
+//bcrypt for hashing passwords, and Mongoose for connecting to a MongoDB database.
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const User = require('./user');
 const port = process.env.PORT || 8080;
 
-
+//Configures the app to use JSON and urlencoded middleware to parse incoming requests and...
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+//...serve static files from the public directory.
 app.use(express.static(path.join(__dirname, 'public')));
-
+//Connects to the MongoDB database specified in the environment variable using Mongoose.
 const mongo_uri = process.env.mongo_uri;
 
 mongoose.connect(mongo_uri, function (err) {
@@ -28,7 +30,9 @@ mongoose.connect(mongo_uri, function (err) {
     }
 });
 
-
+// Defines two API endpoints: /register for registering a new user and /authenticate 
+// for authenticating an existing user. 
+// Both endpoints expect a request body in JSON format and respond with JSON data.
 app.post('/register', function (req, res) {
     const { firstName, lastName, username, password } = req.body;
 
